11b7b39cc6e401ed0b3eeb6078ac920df833f5fe
guilherme.cruz <guilhermecruz3005@gmail.com>
2025-06-11 21:35:33 -0300

    adicionado botão de acessibilidde

--- js/main.js
+++ js/main.js
@@ -5,3 +5,17 @@ document.addEventListener("DOMContentLoaded", () => {
     document.body.classList.toggle("claro");  
 });
 });
+
+/*botão acessibilidade*/ 
+
+let fonteAumentada = false;
+
+document.getElementById("botao-acessibilidade").addEventListener("click", function () {
+  if (!fonteAumentada) {
+    document.body.style.fontSize = "25px"; // aumenta a fonte
+    fonteAumentada = true;
+  } else {
+    document.body.style.fontSize = "16px"; // volta ao normal
+    fonteAumentada = false;
+  }
+});
